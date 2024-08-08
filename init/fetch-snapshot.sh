1d97a1a57b915cd54d0f6f6ea868cbdf6c46bca2
Yorick <yorickdowne@gmail.com>
2024-08-08 23:25:16 +0000

    Fix snapshot extraction

--- init/fetch-snapshot.sh
+++ init/fetch-snapshot.sh
@@ -7,7 +7,6 @@ __project_name="Scroll Docker"
 # Prep l2geth datadir
 if [ -n "${SNAPSHOT}" ] && [ ! -d "${__datadir}/geth/chaindata/" ]; then
   __dont_rm=0
-  mkdir -p "${__datadir}"/geth
   mkdir -p "${__datadir}"/snapshot
   cd "${__datadir}"/snapshot
   eval "__url=${SNAPSHOT}"
@@ -16,13 +15,13 @@ if [ -n "${SNAPSHOT}" ] && [ ! -d "${__datadir}/geth/chaindata/" ]; then
   aria2c -c -x6 -s6 --auto-file-renaming=false --conditional-get=true --allow-overwrite=true "${__url}"
   filename=$(echo "${__url}" | awk -F/ '{print $NF}')
   if [[ "${filename}" =~ \.tar\.zst$ ]]; then
-    pzstd -c -d "${filename}" | tar xvf - -C "${__datadir}"/geth
+    pzstd -c -d "${filename}" | tar xvf - -C "${__datadir}"
   elif [[ "${filename}" =~ \.tar\.gz$ || "${filename}" =~ \.tgz$ ]]; then
-    tar xzvf "${filename}" -C "${__datadir}"/geth
+    tar xzvf "${filename}" -C "${__datadir}"
   elif [[ "${filename}" =~ \.tar$ ]]; then
-    tar xvf "${filename}" -C "${__datadir}"/geth
+    tar xvf "${filename}" -C "${__datadir}"
   elif [[ "${filename}" =~ \.lz4$ ]]; then
-    lz4 -d "${filename}" | tar xvf - -C "${__datadir}"/geth
+    lz4 -d "${filename}" | tar xvf - -C "${__datadir}"
   else
     __dont_rm=1
     echo "The snapshot file has a format that ${__project_name} can't handle."
