f6d968cbd875fcff2ce8fd2280defd1ee40f7c61
George Gatheca <george.gatheca@galaxy.com>
2025-04-30 11:19:18 +0100

    Add p2p port, bootnodes and log level fix (#20)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,38 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Set verbosity
+shopt -s nocasematch
+case ${LOG_LEVEL} in
+  error)
+    __verbosity="--verbosity 1"
+    ;;
+  warn)
+    __verbosity="--verbosity 2"
+    ;;
+  info)
+    __verbosity="--verbosity 3"
+    ;;
+  debug)
+    __verbosity="--verbosity 4"
+    ;;
+  trace)
+    __verbosity="--verbosity 5"
+    ;;
+  *)
+    echo "LOG_LEVEL ${LOG_LEVEL} not recognized"
+    __verbosity=""
+    ;;
+esac
+
+__public_ip="--nat=extip:$(wget -qO- https://ifconfig.me/ip)"
+
+if [ -n "${L2GETH_P2P_BOOTNODES}" ]; then
+  __bootnodes="--bootnodes=${L2GETH_P2P_BOOTNODES}"
+else
+  __bootnodes=""
+fi
+
 if [ "${MPT_NODE}" = "true" ]; then
   __mpt="--scroll-mpt --gcmode archive"
 else
@@ -8,4 +40,4 @@ else
 fi
 
 #shellcheck disable=SC2086
-exec "$@" ${__mpt} ${EXTRAS}
+exec "$@" ${__verbosity} ${__public_ip} ${__bootnodes} ${__mpt} ${EXTRAS}
